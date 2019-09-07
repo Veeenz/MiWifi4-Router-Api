6a112cf7b4c9e5468fe434affbad6ac7744c3eac
Vincenzo Filetti <vzfiletti@gmail.com>
2019-09-07 16:42:45 +0200

    First commit

---
+++ MiRouter.py
@@ -0,0 +1,86 @@
+from util import get_random_mac, get_nonce, get_password_hash, success, fail
+import requests
+
+
+
+class MiRouter():
+
+    def __init__(self, gateway_url="http://miwifi.com"):
+        self.mac_prefix = "e4:46:da"
+        self.login_token = ""
+        self.gateway_url = gateway_url
+        self.random_mac_address = get_random_mac(self.mac_prefix)
+        self.nonce = get_nonce(self.random_mac_address)
+        self.endpoint_url = "{}/cgi-bin/luci/;stok={}/"
+        
+    def login(self, psw):
+        
+        url = "{}/cgi-bin/luci/api/xqsystem/login".format(self.gateway_url)
+        req = requests.post(url, {
+            "username": "admin",
+            "password": get_password_hash(self.nonce, psw),
+            "logtype": 2,
+            "nonce": self.nonce
+        })
+        
+        if(req.status_code == 200):
+            self.login_token = req.json()['token']
+            self.endpoint_url = "{}/cgi-bin/luci/;stok={}/".format(self.gateway_url, self.login_token)
+            return success(message="Logged in!", data=req.json())
+        return fail(message="There was an error while logging in...", data=req.content)
+
+    def get_device_list(self):
+        req = requests.get(self.endpoint_url + "/api/misystem/devicelist")
+        if(req.status_code == 200):
+            return success(message="Device list retrieved!",data=req.json())
+        return fail(message="There was an error while getting device list...", data=req.content)
+    
+
+    def get_wifi_detail(self):        
+        req = requests.get(self.endpoint_url + "/api/xqnetwork/wifi_detail_all")
+        if(req.status_code == 200):
+            data = req.json()            
+            #Setting a ref to "wifiIndex" which can be used for setting the wifi status in the set_wifi method
+            for i, wifi_interface in enumerate(data['info']):
+                wifi_interface['wifiIndex'] = i + 1 
+            return success(message="Wifi interfaces retrieved!", data=data)
+                
+        return fail(message="There was an error while getting the Wifi details...", data=req.content)
+
+    def set_wifi_status(self, wifi_index: int, status=0):
+        """ Set the wifi status of a given interface to 0 or 1
+        Keyword arguments:
+        wifi_index -- Index of interface we want to change the status
+        status -- The status of the WiFi interface (default 0)
+        """
+        if(status != 0 and status != 1):            
+            return fail('Invalid status, send 0 for disable, 1 for enable')
+        
+        wifi_interfaces = self.get_wifi_detail()['data']        
+        target_interface = [interface for interface in wifi_interfaces['info'] if interface['wifiIndex'] == wifi_index]
+        if len(target_interface) > 0:
+            target_interface = target_interface[0]
+        else:
+            return fail('Target Wifi interface not found', data=req.content)
+        payload = {
+            'wifiIndex': target_interface['wifiIndex'],
+            'on': status,
+            'ssid': target_interface['ssid'],
+            'pwd': target_interface['password'],
+            'encryption': target_interface['encryption'],
+            'channel': target_interface['channel'],
+            'bandwidth': target_interface['bandwidth'],
+            'hidden': target_interface['hidden'],
+            'txpwr': target_interface['txpwr']
+        }
+
+        req = requests.post(self.endpoint_url + "/api/xqnetwork/set_wifi", payload)
+        if(req.status_code == 200):
+            return success('Setting the status of WiFi to {}'.format(status), data=req.json())
+        return fail('There was an error while setting the status of WiFi to {}'.format(status), data=req.content)
+
+
+
+        
+    
+
---
+++ util.py
@@ -0,0 +1,32 @@
+import hashlib
+import random
+import time
+
+KEY = "a2ffa5c9be07488bbb04a3a47d3c5f6a" #Stands for xiaoqiang
+
+def sha1(string):
+    return hashlib.sha1(string.encode()).hexdigest()
+
+def get_random_mac(prefix):
+    return prefix+':'.join("%02x"%random.randint(0, 255) for _ in range(3))
+
+
+def get_nonce(mac_address):
+    return "0_{}_{}_9999".format(mac_address, int(time.time()), )
+
+def get_password_hash(nonce, psw):
+    return sha1(nonce + sha1(psw + KEY))
+
+def success(message, data=[]):
+    return {
+        'success': True,
+        'message': message,
+        'data': data
+    }
+
+def fail(message, data=[]):
+    return {
+        'success': False,
+        'message': message,
+        'data': data
+    }
